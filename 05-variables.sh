659ac65943fac40b0a3d8831fa8e5f22195b1da1
Madhan Sekhar <Madhancus@gmail.com>
2023-12-25 00:57:16 -0500

    I am Practicing git

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,4 +6,5 @@ echo "username is :$USERNAME" #I am printing just for validation, you should not
 
 echo "please enter the password"
 read -s PASSWORD #the value entered abouve will be automatically attached to
+
 echo "password is :$PASSOWRD" #the value entered abouve will be automatically attached to
