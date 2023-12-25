550cb5891249bab9c8b427f9db189c1a2d748396
Madhan Sekhar <Madhancus@gmail.com>
2023-12-25 00:51:35 -0500

    I am Practicing git

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "please enter your username"
-read-s USERNAME #the value entered  abouve will be automatically attached to username 
+read -s USERNAME #the value entered  abouve will be automatically attached to username 
 
 echo "username is :$USERNAME"
