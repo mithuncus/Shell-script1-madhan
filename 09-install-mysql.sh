d1b93bdaefbf14ebf4b80dc88173f6f458c0f7d4
Madhan Sekhar <Madhancus@gmail.com>
2024-01-28 15:24:08 -0500

    I am Practicing git

---
+++ 09-install-mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: please run this script with root access"
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end
+yum install mysql -y
+
+if [ $? -ne 0]
+then
+    echo "ERROR: Installing  mysql is failed"
+    exit 1
+
+else
+    echo "installinf mysql is success"
+
+fi
